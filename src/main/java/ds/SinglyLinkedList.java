5b6de82462c8dd6fbb840b1a985d7197f5fe67bc
Mugilan <mugilancodes@gmail.com>
2021-08-23 10:36:41 +0530

    :bento: java: singly linked list - size and is empty methods

--- src/main/java/ds/SinglyLinkedList.java
+++ src/main/java/ds/SinglyLinkedList.java
@@ -1,7 +1,6 @@
 package main.java.ds;
 
 public class SinglyLinkedList {
-
     // Internal node class to represent data
     private static class Node {
         int data; // data of type int
@@ -15,21 +14,44 @@ public class SinglyLinkedList {
         @Override
         public String toString() {
             // if the node has no next node, print null
+            // final Integer integer = next != null ? next.data : null;
             // <Node data: 10 next: 20>
             return "<Node data: " + data + " next: " + (next != null ? next.data : null) + ">";
+            // <Node data: 10>
+            // return "<Node data: " + data + ">";
         }
     }
 
-    public static void main(String[] args) {
-        Node n1 = new Node(10);
-        Node n2 = new Node(20);
-        Node n3 = new Node(30);
+    private Node head; // Denotes the Head of the linked list
+    private int sizeOfList = 0; // Track the size of the list for O(1) operations
 
-        n1.next = n2;
-        n2.next = n3;
-
-        System.out.println(n1);
-        System.out.println(n2);
-        System.out.println(n3);
+    // constructor
+    public SinglyLinkedList() {
+        head = null;
     }
+
+    // Method to display the size of Linked List.
+    // Takes O(1) or Constant Time.
+    public int size() {
+        return sizeOfList;
+    }
+
+    // Method to check whether the Linked List is empty or not.
+    // Takes O(1) or Constant Time.
+    public boolean isEmpty() {
+        return sizeOfList == 0;
+    }
+}
+
+class TestSinglyLinkedList {
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList l = new SinglyLinkedList();
+
+        System.out.println(l.size());
+        System.out.println(l.isEmpty());
+
+    }
+
 }
