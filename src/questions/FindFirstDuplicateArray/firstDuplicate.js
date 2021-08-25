42ef60e037c6031ebdd68c52d76c50122abea54f
Mugilan <mugilancodes@gmail.com>
2021-08-25 18:49:37 +0530

    :bulb: optimized solution

--- src/questions/FindFirstDuplicateArray/firstDuplicate.js
+++ src/questions/FindFirstDuplicateArray/firstDuplicate.js
@@ -1,11 +1,16 @@
+// const withObj = (arr) => {};
+
+// const withSet = (arr) => {};
+
 // Time Complexity: O(n) time or Linear Time
 // Space Complexity: O(n) time or Linear Space
 function firstDuplicate(a) {
   // intialize a empty object
-  let dict = {};
+  // let dict = {};
 
   // loop through the array
   // check whether the value is already in the object. if not add to it
+  /*
   for (let i = 0; i < a.length; i++) {
     // check for presence of the element in object
     if (dict[a[i]]) {
@@ -14,6 +19,23 @@ function firstDuplicate(a) {
 
     // add the item and set its value to true
     dict[a[i]] = true;
+  }
+  */
+
+  // alternate solution using `Set`.
+  // Preferred because there is no need for value in key/value pair of objects.
+  let set = new Set();
+
+  // loops and check each item of the array
+  for (const item of a) {
+    // returns item when set already has the same item.
+    // space complexity for retrieving item is O(1)
+    if (set.has(item)) {
+      return item;
+    }
+
+    // add the item to the set for next loop
+    set.add(item);
   }
 
   // value not found
