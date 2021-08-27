6032885eb84ef97142d73ebfa8f78bcd81580cae
Mugilan <mugilancodes@gmail.com>
2021-08-27 14:04:48 +0530

    load numbers from file

---
+++ src/main/python/algo/sort/load.py
@@ -0,0 +1,17 @@
+# You call this function with the path to a file you want to load.
+# It loads the file contents, converts each line from a string to
+# an integer, and returns them all as a Python list.
+def load_numbers(file_name):
+    numbers = []
+
+    with open(file_name) as f:
+        for line in f:
+            numbers.append(int(line))
+
+    return numbers
+
+
+# USAGE
+# import sys
+# from load import load_numbers
+# numbers = load_numbers(sys.argv[1]) # pass in the file name in the command line
