83b208894c48a34c540f194f43d42c7ee1a8fb7b
Mugilan <mugilancodes@gmail.com>
2021-09-02 08:57:19 +0530

    doubly linked list

--- src/main/ds/linkedlist/doubly/DoublyLinkedList.java
+++ src/main/ds/linkedlist/doubly/DoublyLinkedList.java
@@ -8,19 +8,159 @@ public class DoublyLinkedList {
     }
 
     public static void main(String[] args) {
+        DoublyLinkedList dl = new DoublyLinkedList();
 
-        Node n1 = new Node(10);
-        Node n2 = new Node(20);
-        Node n3 = new Node(30);
+        System.out.println(dl.isEmpty());
 
-        n1.next = n2;
-        n2.prev = n1;
-        n2.next = n3;
-        n3.prev = n2;
+        System.out.println(dl.size());
+        dl.push(20);
+        dl.push(30);
+        dl.push(10);
+        System.out.println(dl.size());
 
-        System.out.println(n1);
-        System.out.println(n2);
-        System.out.println(n3);
+        System.out.println(dl);
+        dl.append(40);
+        System.out.println(dl);
+
+        dl.display();
+
+        dl.search(10);
+        dl.search(25);
+
+        dl.removeFirst();
+        System.out.println(dl);
+
+        dl.removeLast();
+        System.out.println(dl);
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        Node current = head;
+        int count = 0;
+
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+
+    // add at the beginning (prepend)
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+
+        if (head != null) {
+            head.prev = newNode;
+        }
+
+        head = newNode;
+    }
+
+    // add at the end (append)
+    public void append(int data) {
+        Node newNode = new Node(data);
+
+        if (isEmpty()) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = null; // since this is going to be the last node
+
+        // traverse from head to the last node in the list
+        Node lastNode = head;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+
+        lastNode.next = newNode;
+        newNode.prev = lastNode;
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            return;
+        }
+
+        head = head.next;
+        head.prev = null;
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            return;
+        }
+
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+    }
+
+    private boolean isPresent(int key) {
+        if (isEmpty()) {
+            return false;
+        }
+
+        Node current = head;
+
+        while (current != null) {
+            if (current.data == key) {
+                return true;
+            }
+
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    public void search(int key) {
+        if (isPresent(key)) {
+            System.out.println("Key " + key + " is found");
+        } else {
+            System.out.println("Key " + key + " is not found");
+        }
+    }
+
+    @Override
+    public String toString() {
+        Node current = head;
+        StringBuilder sb = new StringBuilder();
+
+        while (current != null) {
+            if (current == head) {
+                sb.append("[Head: ").append(current.data).append("]");
+                if (current.next != null) {
+                    sb.append(" <-");
+                }
+            } else if (current.next == null) {
+                sb.append("-> [Tail: ").append(current.data).append("]");
+            } else {
+                sb.append("-> [").append(current.data).append("] <-");
+            }
+
+            current = current.next;
+        }
+
+        return sb.toString();
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Doubly Linked List is Empty");
+            return;
+        }
+
+        System.out.println(this);
     }
 
     // contains data, and references to previous node and next node
