fd060894fca3317bdf46dac412ba2e7bd2ff8992
Mugilan <mugilancodes@gmail.com>
2021-09-06 21:04:27 +0530

    dp: fibonacci - memoization

--- src/main/dp/Fibonacci.java
+++ src/main/dp/Fibonacci.java
@@ -18,14 +18,6 @@ public class Fibonacci {
     // Iterative / Bottom-Up Approach
     // Takes O(n) time and O(1) space
     public static int fibonacciBottomUp(int n) {
-//        int[] fib = new int[n + 1];
-//        fib[0] = 0;
-//        fib[1] = 1;
-//        for (int i = 2; i < n + 1; i++) {
-//            fib[i] = fib[i - 1] + fib[i - 2];
-//        }
-//        return fib[n];
-
         if (n == 0 || n == 1) {
             return n;
         }
@@ -40,10 +32,29 @@ public class Fibonacci {
         return b;
     }
 
+    // Recursive Memoization / Top-Down Approach
+    // Takes O(n) time and O(n) space
+    public static int fibonacciTopDown(int n, int[] fib) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        if (fib[n] != 0) {
+            return fib[n];
+        }
+        fib[n] = fibonacciTopDown(n - 1, fib) + fibonacciTopDown(n - 2, fib);
+
+        return fib[n];
+
+    }
+
     public static void main(String[] args) {
         int n = 10; // nth position number in fibonacci series
         System.out.println(fibonacciRecursion(n));
         System.out.println(fibonacciBottomUp(n));
+
+        int[] fib = new int[n + 1];
+        System.out.println(fibonacciTopDown(n, fib));
     }
 
 }
