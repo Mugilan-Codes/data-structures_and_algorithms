6b419392714f414d39383db938823b798d1616ac
Mugilan <mugilancodes@gmail.com>
2021-10-09 12:28:13 +0530

    full & perfect binary trees

--- src/main/ds/tree/BinaryTree.java
+++ src/main/ds/tree/BinaryTree.java
@@ -1,19 +1,55 @@
 package main.ds.tree;
 
 public class BinaryTree {
-    static class Node {
-        int data;
-        Node left = null, right = null;
-
-        Node(int data) {
-            this.data = data;
-        }
-    }
-
     Node root;
 
     BinaryTree() {
         root = null;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+
+        System.out.println("Pre-Order Traversal");
+        tree.preorder(tree.root);
+        System.out.println();
+
+        System.out.println("In-Order Traversal");
+        tree.inorder(tree.root);
+        System.out.println();
+
+        System.out.println("Post-Order Traversal");
+        tree.postorder(tree.root);
+        System.out.println();
+
+        if (tree.isFullBinaryTree(tree.root)) {
+            System.out.println("Tree is a Full Binary Tree");
+        } else {
+            System.out.println("Not a Full Binary Tree");
+        }
+
+        if (tree.isPerfectBinaryTree(tree.root)) {
+            System.out.println("Tree is a Perfect Binary Tree");
+        } else {
+            System.out.println("Not a Perfect Binary Tree");
+        }
+    }
+
+    static int depth(Node root) {
+        int level = 0;
+        while (root != null) {
+            level++;
+            root = root.left;
+        }
+        return level;
     }
 
     void preorder(Node root) {
@@ -46,23 +82,51 @@ public class BinaryTree {
         System.out.print(root.data + " ");
     }
 
-    public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
+    // Every parent node/internal node has either two or no children.
+    boolean isFullBinaryTree(Node root) {
+        if (root == null) {
+            return true;
+        }
 
-        tree.root = new Node(1);
-        tree.root.left = new Node(12);
-        tree.root.right = new Node(9);
-        tree.root.left.left = new Node(5);
-        tree.root.left.right = new Node(6);
+        if (root.left == null && root.right == null) {
+            return true;
+        }
 
-        System.out.println("\nPre-Order Traversal");
-        tree.preorder(tree.root);
+        if (root.left != null && root.right != null) {
+            return isFullBinaryTree(root.left) && isFullBinaryTree(root.right);
+        }
 
-        System.out.println("\nIn-Order Traversal");
-        tree.inorder(tree.root);
+        return false;
+    }
 
-        System.out.println("\nPost-Order Traversal");
-        tree.postorder(tree.root);
+    // Every internal node has exactly two child nodes and all the leaf nodes are at the same level.
+    private boolean isPerfectBinaryTree(Node root, int depthOfTree, int currentLevel) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.left == null && root.right == null) {
+            return depthOfTree == currentLevel + 1;
+        }
+        if (root.left == null || root.right == null) {
+            return false;
+        }
+
+        return isPerfectBinaryTree(root.left, depthOfTree, currentLevel + 1) && isPerfectBinaryTree(root.right, depthOfTree, currentLevel+1);
+    }
+
+    boolean isPerfectBinaryTree(Node root) {
+        int depthOfTree = depth(root);
+        return isPerfectBinaryTree(root, depthOfTree, 0);
+    }
+
+    static class Node {
+        int data;
+        Node left = null, right = null;
+
+        Node(int data) {
+            this.data = data;
+        }
     }
 
 }
